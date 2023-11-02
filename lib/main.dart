7d50658f523b9bfd6f5fe0086107e0a130218f04
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-11-02 09:31:40 +0300

    first stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text("Hello World")));
+  runApp(const MaterialApp(home: StartScreen()));
 }
-// const 
+
+// Stateless => Ekranda değişime uğramayacak, UI widget
+// CTRL + .
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Hello World"),
+      ),
+    );
+  }
+}
